7014704f07644646750ef8c651411a9582443ef8
Startive <140018802+Startive@users.noreply.github.com>
2023-10-13 17:14:41 +0100

    add more functonality

--- GJLevelDownloader.py
+++ GJLevelDownloader.py
@@ -2,7 +2,7 @@ import requests
 
 
 print("Hello! Welcome to the GJLevelDownloader.")
-GJUrl = str(input("Please specify the downloadGJLevels URL: "))
+GJUrl = str(input("Please specify the downloadGJLevels URL (1 for regular servers): "))
 GJID = int(input("Please specify the ID of the level: "))
 
 #download levels
@@ -12,9 +12,16 @@ data = {
 }
 
 #request level data and save to file
-req = requests.post(GJUrl, data=data)
-f = open("ID_{}.txt".format(GJID),"w+")
-f.write(req.text)
-f.close()
+if GJUrl == "1":
+    req = requests.post("http://www.boomlings.com/database/downloadGJLevel22.php", data=data)
+    f = open("ID_{}.txt".format(GJID),"w+")
+    f.write(req.text)
+    f.close()
+else:
+    req = requests.post(GJUrl, data=data)
+    f = open("ID_{}.txt".format(GJID),"w+")
+    f.write(req.text)
+    f.close()
+
 
 print("All done. Your file will be saved as 'ID_{}.txt'.".format(GJID))
