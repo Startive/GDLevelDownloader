fb09387732a2c82c3b5265faca5bc29025a015fb
Startive <july2009chris@gmail.com>
2023-10-15 22:12:28 +0100

    modified:   GJLevelDownloader.py

--- GJLevelDownloader.py
+++ GJLevelDownloader.py
@@ -3,7 +3,8 @@ import requests
 
 print("Hello! Welcome to the GJLevelDownloader.")
 GJUrl = str(input("Please specify the downloadGJLevels URL (1 for regular servers): "))
-GJID = int(input("Please specify the ID of the level: "))
+GJID = str(input("Please specify the ID of the level: "))
+
 
 #download levels
 data = {
@@ -11,17 +12,22 @@ data = {
     "secret": "Wmfd2893gb7"
 }
 
-#request level data and save to file
-if GJUrl == "1":
-    req = requests.post("http://www.boomlings.com/database/downloadGJLevel22.php", data=data)
-    f = open("ID_{}.txt".format(GJID),"w+")
-    f.write(req.text)
-    f.close()
-else:
-    req = requests.post(GJUrl, data=data)
-    f = open("ID_{}.txt".format(GJID),"w+")
-    f.write(req.text)
-    f.close()
+#validate if GJID is actually a number (int)
+if GJID.isdigit():
 
+    #request level data and save to file
+    if GJUrl == "1":
+        req = requests.post("http://www.boomlings.com/database/downloadGJLevel22.php", data=data)
+        f = open("ID_{}.txt".format(GJID),"w+")
+        f.write(req.text)
+        f.close()
+    else:
+        req = requests.post(GJUrl, data=data)
+        f = open("ID_{}.txt".format(GJID),"w+")
+        f.write(req.text)
+        f.close()
+else:
+    print("You didn't enter an ID. Rerun and try again.")
+    exit()
 
 print("All done. Your file will be saved as 'ID_{}.txt'.".format(GJID))
